d1a2c0ab6eca3350c544785c07fde56827a02291
Henrique Pesciotti <hpesciotti@gmail.com>
2024-04-18 16:08:49 +0000

    Add data bank  of questions to questions.json

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,201 +1,27 @@
-let allQuestions = [
-    {
-        question: "There has been always a notion that the world was made up of five oceans. However, according to the most recent classifications, there are only three oceans in the world. Choose the alternative that correctly indicates their respective names:",
-        answer: "Pacific, Atlantic and Indian",
-        options: [
-            "Arctic, Indian and Pacific",
-            "Antarctic, Atlantic and Indian",
-            "Pacific, Atlantic and Indian",
-            "Arctic, Antarctic  and Atlantic"
-        ]
-    },
-    {
-        question: "What continent is the only one to span all four hemispheres?",
-        answer: "Russia",
-        options: [
-            "Turkey",
-            "Mongolia",
-            "United States of America",
-            "Russia"
-        ]
-    },
-    {
-        question: "For an area to be considered a desert, what volume of rain must it receive a year?",
-        answer: "Less than 100mm per year",
-        options: [
-            "Less than 250mm per year",
-            "More than 2000mm per year",
-            "Less than 100mm per year",
-            "More than 500mm per year"
-        ]
-    },
-    {
-        question: "Grand Canyon's Rock Layers are predominantly formed by what type of rocks?",
-        answer: "Sedimentary rocks",
-        options: [
-            "Igneous rocks",
-            "Sedimentary rocks",
-            "Metamorfic Rocks",
-            "Volcanic Rocks",
-        ]
-    },
-    {
-        question: "Dublin North is located at what margin of the Liffey river?",
-        answer: "Left margin",
-        options: [
-            "Right margin",
-            "Left margin",
-            "Southern margin",
-            "Western margin margin"
-        ]
-    },
-    {
-        question: "Certain places on planet Earth are home to more extraordinary biodiversity. Which regions have the most remarkable diversity of fauna and flora?",
-        answer: "Tropical forests",
-        options: [
-            "Tropical forests",
-            "Oceans",
-            "Deserts",
-            "Temperate forests"
-        ]
-
-    },
-    {
-        question: "What percentage of the world's water do rivers and lakes hold?",
-        answer: "9%",
-        options: [
-            "1%",
-            "15%",
-            "9%",
-            "35%"
-        ]
-    },
-    {
-        question: "Which of the following events was not related to the Cold War?",
-        answer: "Spanish Civil War",
-        options: [
-            "Spanish Civil War",
-            "Korean War",
-            "Cuban Revolution",
-            "Latin American dictatorships"
-        ]
-    },
-    {
-        question: "Which nation did NOT have a socialist government during the Cold War years?",
-        answer: "South Africa",
-        options: [
-            "Albania",
-            "Poland",
-            "Vietnam",
-            "South Africa"
-        ]
-    },
-    {
-        question: "Africa is divided into two large regions: North Africa and Sub-Saharan Africa. A sociocultural characteristic of North Africa is:",
-        answer: "The predominant population practices Islam.",
-        options: [
-            "The small cultural diversity of the local native groups.",
-            "Colonization of the region by Asian countries.",
-            "Great social equality among the region's inhabitants.",
-            "The predominant population practices Islam."
-        ]
-    },
-    {
-        question: "What is the hottest desert in the world?",
-        answer: "Sahara",
-        options: [
-            "Sahara",
-            "Atacama",
-            "Gobi",
-            "Kalahari"
-        ]
-    },
-    {
-        question: "Africa has an expanding demographic landscape. One indicator that explains Africa's population growth is:",
-        answer: "The high fertility rate.",
-        options: [
-            "Increasing infant mortality.",
-            "Low life expectancy.",
-            "The low replacement rate.",
-            "The high fertility rate."
-        ]
-    },
-    {
-        question: "The Mid-Atlantic ridge results from what kind of tectonic plate boundaries?",
-        answer: "Divergent boundaries",
-        options: [
-            "Divergent boundaries",
-            "Convergent boundary",
-            "Transform boundary",
-            "Subduction zone"
-        ]
-    },
-    {
-        question: "Which tectonic activity formed the Andes Mountains in South America?",
-        answer: "Subduction zone",
-        options: [
-            "Subduction zone",
-            "Transform boundary",
-            "Divergent boundaries",
-            "Rift"
-        ]
-    },
-    {
-        question: "Listed below are the countries and their most polluted cities, except:",
-        answer: "Finland - Helsinki",
-        options: [
-            "Russia - Dzerzhinsk",
-            "Ukraine - Chernobyl",
-            "Finland - Helsinki",
-            "India - Vapi"
-        ]
-    },
-    {
-        question: "Countries located north of the equator belong to what hemisphere?",
-        answer: "Northern Hemisphere",
-        options: [
-            "Western Hemisphere",
-            "Northern Hemisphere",
-            "Southern Hemisphere",
-            "Eastern Hemisphere"
-        ]
-    },
-    {
-        question: "The continent with the highest concentration of fresh water is:",
-        answer: "America",
-        options: [
-            "America",
-            "Asia",
-            "Oceania",
-            "Africa"
-        ]
-    },
-    {
-        question: "What rock outcrops form the important landmark of Giant's Causeway in Northern Ireland?",
-        answer: "Basalt",
-        options: [
-            "Diorite",
-            "Granite",
-            "Marble",
-            "Basalt"
-        ]
-    }
-]
-
 // Set the variables based on elements in the html
 const questionElement = document.getElementById("question");
 const answersButtons = document.getElementById("answer-buttons");
 const nextButton = document.getElementById("btn-next");
 const optionsButtons = document.getElementsByClassName("btn-option");
 
+// Fetch
+
+   fetch('assets/data/questions.json')
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log("error"));
+
+let allQuestions = [];
 // Defines an Array that will play the hole of questions databank in each game.
-let gameQuestions = []
+let gameQuestions = [];
 // Defines an Array to used in the radomization of answer options.
-let questionOptions = []
+let questionOptions = [];
 let currentQuestion;
 // Scoreboard variable
 let currentQuestionIndex = 0;
 let score = 0;
+
+console.log(allQuestions)
 
 /**
  * This function generates the playable questions.
@@ -329,7 +155,7 @@ function displayScore() {
         Best luck next Time!`
     } else if (score <= 6) {
         questionElement.innerHTML = `You scorded ${score} out of 10!
-        That's very good!` 
+        That's very good!`
     } else if (score <= 7) {
         questionElement.innerHTML = `You scorded ${score} out of 10!
         You know your Geography!`
@@ -340,7 +166,7 @@ function displayScore() {
 
     } else {
         questionElement.innerHTML = `You scorded ${score} out of 10! 
-        You aced the GeoQuiz! You deserve a GeoKiss!` 
+        You aced the GeoQuiz! You deserve a GeoKiss!`
     };
     nextButton.innerHTML = "Play Again";
     nextButton.style.display = "block"
